e65f90fbf9b4509c295c91cf546c8d6bad579526
Mario Garzon <marioney@gmail.com>
2021-04-08 18:30:11 +0200

    Change safety observer for models21

--- src/rosgraph_monitor/observers/safety_quality_observer.py
+++ src/rosgraph_monitor/observers/safety_quality_observer.py
@@ -4,6 +4,7 @@ from std_msgs.msg import Float32
 from diagnostic_msgs.msg import DiagnosticStatus, KeyValue
 from sensor_msgs.msg import Imu
 from nav_msgs.msg import Odometry
+from math import sqrt
 
 
 class SafetyQualityObserver(TopicObserver):
@@ -16,15 +17,26 @@ class SafetyQualityObserver(TopicObserver):
     def calculate_attr(self, msgs):
         status_msg = DiagnosticStatus()
 
-        attr = msgs[0].twist.twist.linear.x
-        d_break= ((msgs[0].twist.twist.linear.x**2)/(2*msgs[1].linear_acceleration.x))
+        vel_x = msgs[0].twist.twist.linear.x
+        vel_y = msgs[0].twist.twist.linear.y
 
-       # print (d_break)
-        normalized_safety=1.0
-        if (d_break>msgs[2].data):
-            normalized_safety=msgs[2].data/d_break
-        print ("{0}".format(d_break))
-        print("{0}".format(normalized_safety))
+        velocity = sqrt(vel_x ** 2 + vel_y ** 2)
+
+        # We assume a max acceleration of 1 m/s^2
+        # The minimum value
+
+        d_break = (velocity)/(0.57)
+
+        # print (d_break)
+
+        normalized_safety = 0.0
+
+        if (d_break > msgs[2].data):
+            normalized_safety = 1 - msgs[2].data/d_break
+
+        # print ("d_break: {0}".format(d_break))
+        # print("disntace:{0}".format(msgs[2].data))
+        print("normalized safety:{0}".format(normalized_safety))
         status_msg = DiagnosticStatus()
         status_msg.level = DiagnosticStatus.OK
         status_msg.name = self._id
